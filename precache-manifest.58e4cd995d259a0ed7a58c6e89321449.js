f5ddaf51f748416f20015305a497ec1bce4b2d6f
gmukul01 <mukulbansal01@gmail.com>
2019-03-04 10:57:08 +0700

    Updates

---
+++ precache-manifest.58e4cd995d259a0ed7a58c6e89321449.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "98486ab4f8ef9e6f4e1f",
+    "url": "/static/js/main.98486ab4.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "98486ab4f8ef9e6f4e1f",
+    "url": "/static/css/main.924a0711.chunk.css"
+  },
+  {
+    "revision": "3577233d05155ea47b5c08fe7216ed57",
+    "url": "/index.html"
+  }
+];
